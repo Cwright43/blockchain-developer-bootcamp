1ee0ec0dabb53b7d06404fb6fc33f47bb55ed2f5
Banana Man <cwright43@live.com>
2022-08-25 17:01:09 -0600

    30. Fill Orders

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -76,8 +76,20 @@ const DEFAULT_EXCHANGE_STATE = {
     loaded: false,
     data: []
   },
+
+  cancelleddOrders: {
+    data: []
+  },
+
+  filledOrders: {
+    data: []
+  },
+
   events: []
 }
+
+
+
 
 export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
   let index, data
@@ -162,7 +174,55 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
         }
       }
 
-    //
+   // ----------------------------------------
+   // FILLING ORDERS
+
+
+case 'ORDER_FILL_REQUEST':
+      return {
+        ...state,
+        transaction: {
+          transactionType: 'Fill Order',
+          isPending: true,
+          isSuccessful: false
+        }
+      }
+
+    case 'ORDER_FILL_SUCCESS':
+        
+        // Prevent duplicate orders
+        index = state.filledOrders.data.findIndex(order => order.id.toString() === action.order.id.toString())
+
+        if (index === -1) {
+          data = [...state.filledOrders.data, action.order]
+        } else {
+          data = state.filledOrders.data
+        }
+
+      return {
+        ...state,
+        transaction: {
+          transactionType: 'Fill Order',
+          isPending: false,
+          isSuccessful: true
+        },
+        filledOrders: {
+          ...state.filledOrders,
+          data
+        },
+        events: [action.event, ...state.events]
+      }
+
+    case 'ORDER_FILL_FAIL':
+      return {
+        ...state,
+        transaction: {
+          transactionType: 'Fill Order',
+          isPending: false,
+          isSuccessful: false,
+          isError: true
+        }
+      }
 
     // ------------------------------------------------------------------------------
     // BALANCE CASES
